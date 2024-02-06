bf8347554a77449ecbed5de7c76505f6a674fffd
Ørjan Sømme <ranzor@pop-os.localdomain>
2024-02-06 15:33:58 +0100

    Reworked the movement system into a more accurate tile based movement system

--- Assets/Scripts/playerMovement.gd
+++ Assets/Scripts/playerMovement.gd
@@ -1,11 +1,11 @@
-extends CharacterBody2D
+extends Area2D
 
 @export var speed: int = 40
 @export var tileSize: int = 16
+@onready var tileMap = $"../TileMap"
 @onready var animPlayer = $CharacterAnimation
-var vector
-var movement
-
+@onready var sprite = $PlayerSprite
+var isMoving = false
 var anim = "walk_down"
 # Called when the node enters the scene tree for the first time.
 func _ready():	
@@ -14,31 +14,61 @@ func _ready():
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	movement_input()
-	move_and_slide()
-	
 	if anim:	
 		animPlayer.play(anim)
+	
+	if isMoving:
+		return
+		
+	movement_input()
+
+func _physics_process(delta):
+	if not isMoving:
+		return
+		
+	if global_position == sprite.global_position:
+		isMoving = false
+		return
+	
+	sprite.global_position = sprite.global_position.move_toward(global_position, 0.7)
+	
 
 
 func movement_input():
 	
 	if Input.is_action_pressed("moveUp"):
 		anim = "walk_up"
-		velocity.y = -speed
-		velocity.x = 0
+		move(Vector2.UP)
 	elif Input.is_action_pressed("moveDown"):
 		anim = "walk_down"
-		velocity.y = speed
-		velocity.x = 0
+		move(Vector2.DOWN)
 	elif Input.is_action_pressed("moveLeft"):
 		anim = "walk_left"
-		velocity.x = -speed
-		velocity.y = 0
+		move(Vector2.LEFT)
 	elif Input.is_action_pressed("moveRight"):
 		anim = "walk_right"
-		velocity.x = speed
-		velocity.y = 0
+		move(Vector2.RIGHT)
 	else:
-		velocity.x = 0
-		velocity.y = 0
+		pass
+
+func move(direction: Vector2):
+	# get current tile Vector2i
+	var currentTile: Vector2i = tileMap.local_to_map(global_position)
+	# get target tile Vector2i
+	var targetTile: Vector2i = Vector2i(
+		currentTile.x + direction.x,
+		currentTile.y + direction.y
+	)
+	print("Current Tile:", currentTile, "Target Tile:", targetTile)
+	# get custom data layer from the target tile
+	var tileData: TileData = tileMap.get_cell_tile_data(0, targetTile)
+	var walkableValue = tileData.get_custom_data("walkable")
+	print("Walkable Value:", walkableValue, "Type:", typeof(walkableValue))
+	if walkableValue is bool and walkableValue == false:
+		return
+	# move player
+	isMoving = true
+	
+	global_position = tileMap.map_to_local(targetTile)
+	sprite.global_position = tileMap.map_to_local(currentTile)
+
