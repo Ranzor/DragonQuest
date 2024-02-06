608e419a780a82307e3f150cb85299f00a953992
Ørjan Sømme <ranzor@pop-os.localdomain>
2024-02-06 13:50:51 +0100

    Created the castle room

--- Assets/Scripts/playerMovement.gd
+++ Assets/Scripts/playerMovement.gd
@@ -1,26 +1,44 @@
-extends Node2D
+extends CharacterBody2D
 
-var speed = 40
-var anim = ""
+@export var speed: int = 40
+@export var tileSize: int = 16
+@onready var animPlayer = $CharacterAnimation
+var vector
+var movement
+
+var anim = "walk_down"
 # Called when the node enters the scene tree for the first time.
-func _ready():
+func _ready():	
 	pass # Replace with function body.
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	if Input.is_action_pressed("moveUp"):
-		anim = "walk_up"
-		position.y -= speed * delta
-	elif Input.is_action_pressed("moveDown"):
-		anim = "walk_down"
-		position.y += speed * delta
-	elif Input.is_action_pressed("moveLeft"):
-		anim = "walk_left"
-		position.x -= speed * delta
-	elif Input.is_action_pressed("moveRight"):
-		anim = "walk_right"
-		position.x += speed * delta
+	movement_input()
+	move_and_slide()
 	
 	if anim:	
-		$CharacterAnimation.play(anim)
+		animPlayer.play(anim)
+
+
+func movement_input():
+	
+	if Input.is_action_pressed("moveUp"):
+		anim = "walk_up"
+		velocity.y = -speed
+		velocity.x = 0
+	elif Input.is_action_pressed("moveDown"):
+		anim = "walk_down"
+		velocity.y = speed
+		velocity.x = 0
+	elif Input.is_action_pressed("moveLeft"):
+		anim = "walk_left"
+		velocity.x = -speed
+		velocity.y = 0
+	elif Input.is_action_pressed("moveRight"):
+		anim = "walk_right"
+		velocity.x = speed
+		velocity.y = 0
+	else:
+		velocity.x = 0
+		velocity.y = 0
